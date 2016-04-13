5eca37d7a5178e22b805d2c74dbe8071c1122ae5
tracer-ics2015 <tracer@njuics.org>
2016-04-13 12:43:13 +0800

    >  compile NEMU 13307130265 yue Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt20-1+deb8u4 (2016-02-29) i686 GNU/Linux  12:43:13 up 2 days, 13:07,  1 user,  load average: 0.09, 0.22, 0.18 a4b0c55116355dae85ae02da9fea4e40c619be81

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -176,13 +176,6 @@ int expr(char *e, bool *success) {
 		suffix[nr_suffix++] = stack[--top];
 	}
 	
-	Log("Suffix is :");
-	for (i = 0; i < nr_suffix; ++i) {
-		if (suffix[i].type == 0) printf("%d ", suffix[i].value);
-		else printf("%s ", rules[suffix[i].oper].regex);
-	}
-	puts("");
-
 	for (i = 0; i < nr_suffix; ++i) {
 		if (suffix[i].type == 0) {
 			stack[top++] = suffix[i];
