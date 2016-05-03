65db373c46abf93b265fbd315b6052c0c29cb673
tracer-ics2015 <tracer@njuics.org>
2016-05-03 19:42:51 +0800

    >  compile NEMU 13307130265 yue Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  19:42:51 up 4 days,  7:33,  1 user,  load average: 0.13, 0.15, 0.16 d01fb2778c236b8d717e665eef8d02a76bda7225

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -32,8 +32,9 @@ make_helper(concat(decode_si_, SUFFIX)) {
 	 *
 	op_src->simm = ???
 	 */
-	panic("please implement me");
-
+	op_src->size = DATA_BYTE;
+	op_src->simm = (int32_t) instr_fetch(eip, DATA_BYTE);
+	//panic("please implement me");
 	op_src->val = op_src->simm;
 
 #ifdef DEBUG
