873ee00d85637acf85fdf2cf40d45a6086aa9f64
tracer-ics2015 <tracer@njuics.org>
2016-05-03 18:05:03 +0800

    >  compile NEMU 13307130265 yue Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  18:05:03 up 4 days,  5:56,  1 user,  load average: 0.10, 0.05, 0.08 6137a20978177bca11067938d85da041b261c648

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -29,8 +29,8 @@ make_group(group1_v,
 
 /* 0x83 */
 make_group(group1_sx_v,
-	inv, inv, and_i2rm_b, inv, 
-	or_i2rm_b, inv, inv, xor_i2rm_b)
+	inv, inv, and_i2rm_v, inv, 
+	or_i2rm_v, inv, inv, xor_i2rm_v)
 
 /* 0xc0 */
 make_group(group2_i_b,
