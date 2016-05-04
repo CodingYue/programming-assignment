1d00edf513f416bc63d2d5c9e8900710955d9ade
tracer-ics2015 <tracer@njuics.org>
2016-05-04 20:28:26 +0800

    >  compile NEMU 13307130265 yue Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  20:28:25 up 5 days,  8:19,  1 user,  load average: 0.03, 0.13, 0.17 f447a37bf975c7e3111e05739f4cd74468a343cb

--- nemu/src/cpu/exec/jump/jump.c
+++ nemu/src/cpu/exec/jump/jump.c
@@ -1,7 +1,7 @@
 #include "cpu/exec/helper.h"
 
 make_helper(call) {
-	uint32_t rel;
+	int32_t rel;
 	int len = ops_decoded.is_data_size_16 ? 2 : 4;
 	rel = instr_fetch(eip + 1, len);
 	
@@ -12,13 +12,13 @@ make_helper(call) {
 }
 
 make_helper(jmp_b) {
-	uint32_t rel8 = instr_fetch(eip + 1, 1);
+	int32_t rel8 = instr_fetch(eip + 1, 1);
 	cpu.eip += rel8;
 	return 2;
 }
 
 make_helper(jmp_v) {
-	uint32_t rel;
+	int32_t rel;
 	int len = ops_decoded.is_data_size_16 ? 2 : 4;
 	rel = instr_fetch(eip + 1, len);
 	cpu.eip += rel;
@@ -29,25 +29,25 @@ make_helper(jmp_v) {
 /* condition jmp */
 
 make_helper(je) {
-	uint32_t rel8 = instr_fetch(eip + 1, 1);
+	int32_t rel8 = instr_fetch(eip + 1, 1);
 	if (ZFLAG) cpu.eip += rel8;
 	return 2;
 }
 
 make_helper(jbe) {
-	uint32_t rel8 = instr_fetch(eip + 1, 1);
+	int32_t rel8 = instr_fetch(eip + 1, 1);
 	if (ZFLAG || CFLAG) cpu.eip += rel8;
 	return 2;
 }
 
 make_helper(jne) {
-	uint32_t rel8 = instr_fetch(eip + 1, 1);
+	int32_t rel8 = instr_fetch(eip + 1, 1);
 	if (!ZFLAG) cpu.eip += rel8;
 	return 2;	
 }
 
 make_helper(jle) {
-	uint32_t rel8 = instr_fetch(eip + 1, 1);
+	int32_t rel8 = instr_fetch(eip + 1, 1);
 	if (ZFLAG || SFLAG != OFLAG) cpu.eip += rel8;
 	return 2;
 }
