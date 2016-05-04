c1cf4e155dcc8b989a0618e0ff787b34f452b1d4
tracer-ics2015 <tracer@njuics.org>
2016-05-04 16:31:21 +0800

    >  compile NEMU 13307130265 yue Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  16:31:21 up 5 days,  4:22,  1 user,  load average: 0.53, 0.33, 0.20 4d373bdae3427b50d71892626950a2d8b95a3904

--- nemu/src/cpu/exec/stack/push.c
+++ nemu/src/cpu/exec/stack/push.c
@@ -1,12 +1,6 @@
 #include "cpu/exec/helper.h"
 #include "cpu/decode/modrm.h"
 
-make_helper(push_ebp) {
-	cpu.esp = cpu.esp - 0x4;
-	swaddr_write(cpu.esp, 4, cpu.ebp);
-	return 1;
-}
-
 #define DATA_BYTE 1
 #include "push-template.h"
 #undef DATA_BYTE
@@ -22,3 +16,4 @@ make_helper(push_ebp) {
 
 make_helper_v(push_rm);
 make_helper_v(push_i);
+make_helper_v(push_r);
--- nemu/src/cpu/exec/stack/push.h
+++ nemu/src/cpu/exec/stack/push.h
@@ -1,8 +1,7 @@
 #ifndef __PUSH_H__
 #define __PUSH_H__
 
-make_helper(push_ebp);
-
 make_helper(push_rm_v);
+make_helper(push_r_v);
 
 #endif
