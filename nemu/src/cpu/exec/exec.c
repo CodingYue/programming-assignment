6c54fc9a0c924d4b06c8f81cfcb15c78218c3292
tracer-ics2015 <tracer@njuics.org>
2016-05-05 21:00:34 +0800

    >  compile NEMU 13307130265 yue Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  21:00:34 up 6 days,  8:51,  1 user,  load average: 0.09, 0.13, 0.26 f41ef47d229cd5c967a878e5c0ce271ea581c75

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -153,7 +153,7 @@ helper_fun opcode_table [256] = {
 /* 0xe4 */	inv, inv, inv, inv,
 /* 0xe8 */	call, jmp_v, inv, jmp_b,
 /* 0xec */	inv, inv, inv, inv,
-/* 0xf0 */	inv, inv, inv, inv,
+/* 0xf0 */	inv, inv, inv, rep,
 /* 0xf4 */	inv, inv, group3_b, group3_v,
 /* 0xf8 */	inv, inv, inv, inv,
 /* 0xfc */	inv, inv, group4, group5
@@ -220,7 +220,7 @@ helper_fun _2byte_opcode_table [256] = {
 /* 0xe4 */	inv, inv, inv, inv,
 /* 0xe8 */	inv, inv, inv, inv,
 /* 0xec */	inv, inv, inv, inv,
-/* 0xf0 */	inv, inv, inv, rep,
+/* 0xf0 */	inv, inv, inv, inv,
 /* 0xf4 */	inv, inv, inv, inv,
 /* 0xf8 */	inv, inv, inv, inv,
 /* 0xfc */	inv, inv, inv, inv
