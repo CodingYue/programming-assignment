2ea6780bd8ff50d78353354aa3fa35ce2db6b79f
tracer-ics2015 <tracer@njuics.org>
2016-05-06 16:13:17 +0800

    >  compile NEMU 13307130265 yue Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  16:13:16 up 7 days,  4:04,  1 user,  load average: 0.00, 0.02, 0.05 3f2378da20d273efaa6280ec2d93ba401144f62b

--- nemu/src/cpu/reg.c
+++ nemu/src/cpu/reg.c
@@ -57,7 +57,7 @@ void reg_test() {
 
 void update_EFLAGS(uint64_t result) {
 	cpu.EFLAGS = 0x2;
-	cpu.EFLAGS |= (result >> 32 & 1) << 0; // carry flag.
+	cpu.EFLAGS |= (result >> 32 & 1) << 0;
 	cpu.EFLAGS |= (result & 1) << 2;
 	cpu.EFLAGS |= (!result) << 6;
 	cpu.EFLAGS |= (result >> 31 & 1) << 7;
