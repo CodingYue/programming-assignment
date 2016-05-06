d42d493f75ef8dad3022da260bfe58c15a733998
tracer-ics2015 <tracer@njuics.org>
2016-05-06 21:44:17 +0800

    >  run 13307130265 yue Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  21:44:17 up 7 days,  9:35,  1 user,  load average: 0.41, 0.14, 0.09 1692d020aff85ca7daf515bb47c30d90822766cf

--- testcase/src/wanshu.c
+++ testcase/src/wanshu.c
@@ -4,7 +4,7 @@ int ans[] = {6, 28, 496};
 
 int main() {
 	int n, sum, i, k = 0;
-	for(n = 1; n < 30; n ++) {
+	for(n = 1; n < 100; n ++) {
 		sum = 0;
 		for(i = 1; i < n; i ++) {
 			if(n % i == 0) {
