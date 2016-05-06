6517bb16a429fe0badf46eeb9bd15119a390c772
tracer-ics2015 <tracer@njuics.org>
2016-05-06 21:44:08 +0800

    >  run 13307130265 yue Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  21:44:08 up 7 days,  9:35,  1 user,  load average: 0.48, 0.15, 0.09 999c4bda7b3cbad8c2578c81be64a125fd3d25eb

--- testcase/src/wanshu.c
+++ testcase/src/wanshu.c
@@ -4,7 +4,7 @@ int ans[] = {6, 28, 496};
 
 int main() {
 	int n, sum, i, k = 0;
-	for(n = 1; n < 500; n ++) {
+	for(n = 1; n < 30; n ++) {
 		sum = 0;
 		for(i = 1; i < n; i ++) {
 			if(n % i == 0) {
@@ -18,7 +18,7 @@ int main() {
 		}
 	}
 
-	nemu_assert(k == 3);
+	nemu_assert(k == 2);
 
 	HIT_GOOD_TRAP;
 
