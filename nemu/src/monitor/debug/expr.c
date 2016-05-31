aa060afa994b13841818da950fd5b106875e109e
tracer-ics2015 <tracer@njuics.org>
2016-05-31 19:54:27 +0800

    >  compile NEMU 13307130265 yue Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  19:54:27 up  7:02,  1 user,  load average: 0.18, 0.14, 0.14 f6acb1a81c4926b5477b9fa7fe42a203c119ca1

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -33,7 +33,7 @@ static struct rule {
 	{"\\(", L_brackets},
 	{"\\)", R_brackets},
 	{"==", EQUAL},						// equal
-	{"[a-z]+", VARIABLE}
+	{"[a-z|\\_]+", VARIABLE}
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
