bcd647c6e2af990be2e8db77b13d552f09c8c0d3
tracer-ics2015 <tracer@njuics.org>
2016-06-13 15:59:20 +0800

    >  compile NEMU 13307130265 yue Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  15:59:20 up 6 days,  2:05,  2 users,  load average: 0.08, 0.05, 0.05 5f87e8a33eb6c37dd224f81b22f35b6723e2313d

---
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -0,0 +1,23 @@
+#include "cpu/exec/template-start.h"
+
+#define instr adc
+
+static void do_execute() {
+	DATA_TYPE dest = op_dest->val;
+	DATA_TYPE src = op_src->val;
+	uint64_t result = (uint64_t) src + dest + CFLAG;
+	OPERAND_W(op_dest, (DATA_TYPE) result);
+
+	update_EFLAGS(result, DATA_BYTE);
+	print_asm_template2();
+
+}
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+#include "cpu/exec/template-end.h"
