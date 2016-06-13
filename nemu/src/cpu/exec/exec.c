dd90e708f9b31c86c3240267c62620feaf459410
tracer-ics2015 <tracer@njuics.org>
2016-06-13 16:22:19 +0800

    >  compile NEMU 13307130265 yue Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  16:22:19 up 6 days,  2:28,  2 users,  load average: 0.05, 0.04, 0.05 61175f0576fcd6395e57423b5646ccf09800cd93

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -194,7 +194,7 @@ helper_fun _2byte_opcode_table [256] = {
 /* 0x7c */	inv, inv, inv, inv, 
 /* 0x80 */	inv, inv, inv, inv,
 /* 0x84 */	je_v, inv, inv, inv,
-/* 0x88 */	inv, inv, inv, inv, 
+/* 0x88 */	inv, inv, ja_v, inv, 
 /* 0x8c */	jl_v, inv, jle_v, inv, 
 /* 0x90 */	inv, inv, inv, inv,
 /* 0x94 */	inv, set_ne, inv, inv,
